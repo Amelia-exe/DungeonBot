24491294953032dd2ef4137d53508bd1c1654f87
Amelia-exe <68903836+Amelia-exe@users.noreply.github.com>
2020-08-01 00:32:10 +0100

    Files moved.
    The files in this update were just moved into the correct directories.
    
    init files were deleted.
    imports updated.

--- files/utils/embed_builder.py
+++ files/utils/embed_builder.py
@@ -6,26 +6,6 @@ from enum import Enum
 from discord import Color, Embed
 
 
-class EmbedColor(Enum):
-
-    BLUE = Color(0x004080)
-    DARK_BLUE = Color(0x004080)
-    NAVY_BLUE = Color(0x000040)
-    TEAL = Color(0x008080)
-    ORANGE = Color(0xff8040)
-    RED = Color(0xff0000)
-    WINE_RED = Color(0x804040)
-    MAROON = Color(0x400000)
-    GREEN = Color(0x004000)
-    TURQUOISE = Color(0x008080)
-    PURPLE = Color(0x8000ff)
-    PINK = Color(0xff00ff)
-    DARK_PINK = Color(0x800040)
-
-    def __index__(self):
-        return operator.index(self.value.value)
-
-
 # TODO: Make use of embed template in bot
 class EmbedBuilder:
 
